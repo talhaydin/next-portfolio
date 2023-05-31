1c747f1a56cecc42fb93cfb6147cf98a8e3e0286
talhaydin <mt.aydin@outlook.com>
2023-05-31 19:52:53 +0300

    projects section and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
+import ProjectsSection from "@/components/ProjectsSection";
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutSection />
+      <ProjectsSection />
     </main>
   );
 }
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SlideUp from "./SlideUp";
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
+
+const projects = [
+  {
+    name: "Rtk Movie App",
+    description:
+      "Rtk Movie App is a web application that connects to OMDb API and displays movies,tv shows and their details based on user search.",
+    image: "/rtk.png",
+    github: "https://github.com/talhaydin/rtkmovies",
+    link: "https://rtkmovieapp.vercel.app/",
+  },
+  {
+    name: "Imagery",
+    description:
+      "Imagery is a simple, personal web photo blog built with Next. ",
+    image: "/imagery.png",
+    github: "https://github.com/talhaydin/imagery",
+    link: "https://imageryart.vercel.app/",
+  },
+];
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28 mx-10">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <Link href={project.link} target="blank">
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={800}
+                        height={800}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
