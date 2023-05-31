e353667f8b04b582c284a2343297b9fcd94c6b69
talhaydin <mt.aydin@outlook.com>
2023-05-31 16:12:00 +0300

    about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
